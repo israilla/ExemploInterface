47c4996422c07f8b45675315bb5f4d61aa5dbe22
Israilla <israillacaroline@hotmail.com>
2020-03-23 22:47:24 -0300

    Criação de um pequeno programa utilizando interface

---
+++ Exemplo_Interface/Colaborador.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exemplo_Interface
+{
+    class Colaborador
+    {
+        interface IReajuste
+        {
+            double Reajuste();
+        }
+        public class ReajusteColaborador : IReajuste
+        {
+            public string nomeColaborador;
+            public double salarioColaborador;
+
+            public double Reajuste()
+            {
+                salarioColaborador = salarioColaborador * 1.50;
+
+                return salarioColaborador;
+            }
+        }
+
+    }
+}
---
+++ Exemplo_Interface/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exemplo_Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Colaborador.ReajusteColaborador objReajusteColaborador =
+                new Colaborador.ReajusteColaborador();
+
+            Console.WriteLine("Entre com o nome");
+            objReajusteColaborador.nomeColaborador = Console.ReadLine();
+
+            Console.WriteLine("Entre com o salário");
+            objReajusteColaborador.salarioColaborador = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Antes do Reajuste: ");
+            Console.WriteLine("\nNome: " + objReajusteColaborador.nomeColaborador);
+            Console.WriteLine("\nSalário: R$ " + objReajusteColaborador.salarioColaborador);
+
+            objReajusteColaborador.Reajuste();
+
+            Console.WriteLine("\nDepois do Reajuste: ");
+            Console.WriteLine("\nNome: " + objReajusteColaborador.nomeColaborador);
+            Console.WriteLine("\nSalário: R$ " + objReajusteColaborador.salarioColaborador);
+
+            Console.ReadKey();
+        }
+    }
+}
